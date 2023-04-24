e7f295ff814036358f852ca7720f31ae7a90abbc
Mr. Golanka <sgolanka@gmail.com>
2023-04-24 13:06:41 -0400

    created fetchdata function

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+//remember to revoke this after use
+const API_KEY = "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"
 
+async function fetchData(){
+  const response = await fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers: {
+      Authorization: Bearer ${API_KEY},
+      "Content-Type": "application/json"
+    },
+      body: JSON.stringify({
+                               model: "text-davinci-003",
+                               prompt: "hello, how are you today?"
+                               max_token: 7
+                               })                         
+  })
+  const data = await response.json()
+  console.log(data)
+}
